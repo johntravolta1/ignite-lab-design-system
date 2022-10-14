d0d136ccffce39dc0b67a921a3cc3afe5bad3eaa
John Travolta <m3u0v0@gmail.com>
2022-10-14 11:03:47 -0300

    fim da aula 3

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 
+import { Envelope, Lock } from 'phosphor-react'
+import { Button } from './components/Button'
+import { Checkbox } from './components/Checkbox'
+import { Heading } from './components/Heading'
+import { Text } from './components/Text'
+import { TextInput } from './components/TextInput'
+import { Logo } from './Logo'
+import { SignIn } from './pages/SignIn'
 import './styles/global.css'
 
 export function App() {
   return (
-    <>
-      <h1 className='font-bold text-2xl text-violet-100'>Hello world</h1>
-      <button className='bg-cyan-500 font-medium px-4 py-2 rounded text-white hover:bg-violet-600'>Enviar</button>
-    </>
+    <SignIn></SignIn>
   )
 }
 
